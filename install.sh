2ad341dbb1a44faac839ea3eaa6c5e66013d516d
Ed Zynda <ezynda3@gmail.com>
2025-03-20 20:51:32 +0300

    Update config

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ detect_platform() {
 
   ARCH="$(uname -m)"
   case "${ARCH}" in
-    x86_64|amd64)  ARCH="x86_64";;
+    x86_64|amd64)  ARCH="amd64";;
     arm64|aarch64) ARCH="arm64";;
     *)             echo -e "${RED}Unsupported architecture: ${ARCH}${NC}" && exit 1;;
   esac
